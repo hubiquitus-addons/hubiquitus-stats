42c74c6a50c491471c3fcc033697f13406969ddf
etissieres <etienne.tissieres@gmail.com>
2014-02-24 14:54:07 +0100

    fix path resolution

--- sample/launcher1.js
+++ sample/launcher1.js
@@ -1,7 +1,7 @@
 var hubiquitus = require('hubiquitus-core');
 var logger = hubiquitus.logger('hubiquitus:core:samples');
 hubiquitus.logger.enable('hubiquitus:*');
-var stats = require(__dirname + '/../../index');
+var stats = require(__dirname + '/../index');
 
 stats.start({stats: {enabled: 'true', host: 'localhost', port: 5555}});
 
